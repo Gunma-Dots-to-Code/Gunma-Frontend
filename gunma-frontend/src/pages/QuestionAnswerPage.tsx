ffecae036c648ba18ec040eab0f521af203b16b3
KanaFujitani <cattleya.leya0@gmail.com>
2024-03-15 17:44:12 +0900

    feat: add components

--- gunma-frontend/src/pages/QuestionAnswerPage.tsx
+++ gunma-frontend/src/pages/QuestionAnswerPage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Header from '../components/Header';
 import UserCard from '../components/UserCard';
+import Question from '../components/Question';
 
 const QuestionAnswerPage = () => {
   return (
     <div>
       <h1>Title</h1>
-      <UserCard />
+      <Header />
+      <Question />
     </div>
   );
 };
