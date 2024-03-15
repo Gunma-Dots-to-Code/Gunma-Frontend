f4a7af857a7f03cf74cbdb5935e711d1a24bf390
KanaFujitani <cattleya.leya0@gmail.com>
2024-03-15 18:22:19 +0900

    add: components

--- gunma-frontend/src/pages/QuestionAnswerPage.tsx
+++ gunma-frontend/src/pages/QuestionAnswerPage.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from '../components/Header';
 import UserCard from '../components/UserCard';
 import Question from '../components/Question';
+import Answer from '../components/Answer';
 
 const QuestionAnswerPage = () => {
   return (
     <div>
-      <h1>Title</h1>
       <Header />
       <Question />
+      <Answer />
+      <br />
+      <Answer />
     </div>
   );
 };
