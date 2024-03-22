189a491f3c521d1643269f78a4b908a1088b5cbe
hikari-8 <kanbarusuruga0127@gmail.com>
2024-03-22 16:53:21 +0900

    feat/パンクズのuiを 追加

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,65 +1,22 @@
 import React from "react";
+import BreadCrumb from "./BreadCrumb";
 
 const Header = () => {
 	return (
-		<div
-			x-data="{ open: false }"
-			className="flex flex-col py-5 md:items-center md:justify-between md:flex-row md:px-6 lg:px-8 border-b px-auto w-full"
-		>
-			<div className="flex flex-row items-center justify-between lg:justify-start">
-				<a
-					className="text-lg font-bold tracking-tighter text-[#448cea] transition duration-500 ease-in-out transform tracking-relaxed lg:pr-8"
-					href="/"
-				>
-					Brain Share
-				</a>
-			</div>
-
-			<nav className="flex-col items-center flex-grow hidden pb-4 border-[#448cea] md:pb-0 md:flex md:justify-end md:flex-row lg:border-l-2 lg:pl-2">
-				<div
-					onClick={() => {}}
-					className="relative"
-					x-data="{ open: false }"
-				>
-					<button className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm text-left text-gray-500 md:w-auto md:inline md:mt-0 hover:text-blue-600 focus:outline-none focus:shadow-outline">
-						<span>メニュー</span>
-						<svg
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							className="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1 rotate-0"
-						>
-							<path
-								fillRule="evenodd"
-								d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-							></path>
-						</svg>
-					</button>
-					<div
-						className="absolute right-0 z-30 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48"
-						style={{ display: "none" }}
+		<div>
+			{/* TODO: ヘッダーとパンクズリストをw-2xlの左揃えで */}
+			<div className="flex flex-col py-5 px-6 lg:px-8 border-b">
+				<div className="flex items-center justify-between lg:justify-start max-w-2xl mx-auto">
+					<a
+						className="text-lg font-bold tracking-tighter text-[#448cea] transition duration-500 ease-in-out transform tracking-relaxed lg:pr-8 w-full text-left"
+						href="/"
 					>
-						<div className="px-2 py-2 bg-white rounded-md shadow">
-							<a
-								className="block px-4 py-2 mt-2 text-sm text-gray-500 md:mt-0 hover:text-blue-600 focus:outline-none focus:shadow-outline"
-								href="#"
-							>
-								Link #1
-							</a>
-							<a
-								className="block px-4 py-2 mt-2 text-sm text-gray-500 md:mt-0 hover:text-blue-600 focus:outline-none focus:shadow-outline"
-								href="#"
-							>
-								Link #2
-							</a>
-							<a
-								className="block px-4 py-2 mt-2 text-sm text-gray-500 md:mt-0 hover:text-blue-600 focus:outline-none focus:shadow-outline"
-								href="#"
-							>
-								Link #3
-							</a>
-						</div>
-					</div>
+						Brain Share
+					</a>
 				</div>
+			</div>
+			<nav className="flex border-b" aria-label="Breadcrumb">
+				<BreadCrumb />
 			</nav>
 		</div>
 	);
