ce1d438b3d75c806702e2f4f746faae896db01f8
KanaFujitani <cattleya.leya0@gmail.com>
2024-03-23 01:53:22 +0900

    feat: ステータスの作成

--- src/components/answer/QuestionCard.tsx
+++ src/components/answer/QuestionCard.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import UserCard from "./UserCard";
+import Status from "../common/Status";
 
 const Question = ({ question }: { question: Question }) => {
   const date = new Date();
@@ -16,15 +17,16 @@ const Question = ({ question }: { question: Question }) => {
     <section>
       <div className="flex flex-col items-start px-5 py-8 mx-auto max-w-7xl sm:px-6 lg:px-8 mt-5">
         <div className="w-full max-w-2xl mx-auto prose text-left prose-blue">
-          <div className="w-full mb-8 mx-auto">
-            <h1 className="text-2xl mb-5 font-semibold">
-              {question.QuestionTitle}
-            </h1>
-            <p className="mb-5 text-xs text-gray-400">
-              相談日: {formattedDate}
-            </p>
-            <p className="mt-10">{question.QuestionContent}</p>
+          <div className="w-full mb-8 mx-auto flex items-center justify-between">
+            <div>
+              <h1 className="text-2xl mb-2 font-semibold">
+                {question.QuestionTitle}
+              </h1>
+              <p className="text-xs text-gray-400">相談日: {formattedDate}</p>
+            </div>
+            <Status questionID={question.ID} /> {/* questionIDを渡す */}
           </div>
+          <p>{question.QuestionContent}</p>
           <div className="flex justify-end">
             <UserCard userName={question.User.Username} isQuestioner={true} />
           </div>
