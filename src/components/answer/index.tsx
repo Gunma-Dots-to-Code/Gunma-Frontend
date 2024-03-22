bc826616427806cdfe9cb403a58d812c3da4b66e
kakikuu <s2222055@stu.musashino-u.ac.jp>
2024-03-23 00:42:01 +0900

    fix:質問と回答ページのUIを修正

--- src/components/answer/index.tsx
+++ src/components/answer/index.tsx
@@ -5,40 +5,42 @@ import Question from "./QuestionCard";
 import CreateAnswerCard from "./CreateAnswerModal";
 
 const QuestionAnswer = ({
-	question,
-	answers,
+  question,
+  answers,
 }: {
-	question: Question;
-	answers: Answer[];
+  question: Question;
+  answers: Answer[];
 }) => {
-	const [showAnswerForm, setShowAnswerForm] = useState(false);
+  const [showAnswerForm, setShowAnswerForm] = useState(false);
 
-	const handleAnswerClick = () => {
-		setShowAnswerForm(true);
-	};
-	return (
-		<Layout>
-			<Question question={question} />
-			<div className="text-center my-8">
-				<button
-					onClick={handleAnswerClick}
-					className="bg-primaryBlue text-white px-6 py-2 rounded hover:bg-primary-dark font-semibold"
-				>
-					質問に回答する
-				</button>
-				{showAnswerForm && (
-					<CreateAnswerCard
-						questionId={question.ID}
-						questionTitle={question.QuestionTitle}
-						questionContent={question.QuestionContent}
-						categoryId={question.CategoryID}
-						onClose={() => setShowAnswerForm(false)}
-					/>
-				)}
-			</div>
-			<AnswerCard answers={answers} />
-		</Layout>
-	);
+  const handleAnswerClick = () => {
+    setShowAnswerForm(true);
+  };
+  return (
+    <Layout>
+      <div className="border-b border-gray-300 mb-12">
+        <Question question={question} />
+      </div>
+      <div className="text-center mb-5">
+        <button
+          onClick={handleAnswerClick}
+          className="bg-primaryBlue text-white px-6 py-2 rounded font-semibold hover:bg-primary-dark"
+        >
+          質問に回答する
+        </button>
+        {showAnswerForm && (
+          <CreateAnswerCard
+            questionId={question.ID}
+            questionTitle={question.QuestionTitle}
+            questionContent={question.QuestionContent}
+            categoryId={question.CategoryID}
+            onClose={() => setShowAnswerForm(false)}
+          />
+        )}
+      </div>
+      <AnswerCard answers={answers} />
+    </Layout>
+  );
 };
 
 export default QuestionAnswer;
